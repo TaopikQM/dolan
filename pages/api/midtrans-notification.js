3de1cad6de6ca0597c09150037d1516067953862
TaopikQM <taopik61318@gmail.com>
2024-06-28 13:43:02 +0700

    Update midtrans-notification.js

--- pages/api/midtrans-notification.js
+++ pages/api/midtrans-notification.js
@@ -1,24 +1,25 @@
-// pages/api/midtrans-notification.js
-
 import { admin } from '../../lib/firebase-admin';
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method === 'POST') {
     const notificationData = req.body;
 
-    const database = admin.database();
-    const notificationsRef = database.ref('notifications');
+    try {
+      const database = admin.database();
+      const notificationsRef = database.ref('notifications');
+      const newNotificationRef = notificationsRef.push();
 
-    const newNotificationRef = notificationsRef.push();
-    newNotificationRef.set(notificationData)
-      .then(() => {
-        console.log('Notification data saved successfully');
-        res.status(200).json({ message: 'Notification received and saved successfully' });
-      })
-      .catch((error) => {
-        console.error('Error saving notification data:', error);
-        res.status(500).json({ error: 'Failed to save notification data' });
+      await newNotificationRef.set({
+        ...notificationData,
+        timestamp: admin.database.ServerValue.TIMESTAMP
       });
+
+      console.log('Notification data saved successfully');
+      res.status(200).json({ message: 'Notification received and saved successfully' });
+    } catch (error) {
+      console.error('Error saving notification data:', error);
+      res.status(500).json({ error: 'Failed to save notification data' });
+    }
   } else {
     res.setHeader('Allow', ['POST']);
     res.status(405).end(`Method ${req.method} Not Allowed`);
